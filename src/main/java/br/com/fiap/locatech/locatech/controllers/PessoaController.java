32f3afd87e2716275e84fdc492f8f068839b376f
Rafael Teles Espindola <rafatespindola@gmail.com>
2026-03-16 21:40:10 -0300

    Adicionado Pessoas

---
+++ src/main/java/br/com/fiap/locatech/locatech/controllers/PessoaController.java
@@ -0,0 +1,92 @@
+package br.com.fiap.locatech.locatech.controllers;
+
+import br.com.fiap.locatech.locatech.entities.Pessoa;
+import br.com.fiap.locatech.locatech.services.PessoaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+
+    private static final Logger logger = LoggerFactory.getLogger(PessoaController.class);
+
+    private final PessoaService pessoaService;
+
+    public PessoaController(PessoaService pessoaService) {
+        this.pessoaService = pessoaService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Pessoa>> findAllPessoas (
+            @RequestParam("page") int page,
+            @RequestParam("size") int size
+    ) {
+        logger.info("/pessoas");
+        var pessoas = this.pessoaService.findAllPessoas(page, size);
+        return ResponseEntity.ok(pessoas);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Pessoa>> findPessoa (
+            @PathVariable("id") Long id
+    ) {
+        logger.info("/pessoas");
+        var pessoa = this.pessoaService.findPessoaById(id);
+        return ResponseEntity.ok(pessoa);
+    }
+
+    @PostMapping
+    public ResponseEntity<Pessoa> savePessoa(
+            @RequestBody Pessoa pessoa
+    ) {
+        logger.info("POST => /pessoas");
+        this.pessoaService.savePessoa(pessoa);
+        return ResponseEntity.status(201).build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> updatePessoa (
+            @PathVariable Long id,
+            @RequestBody Pessoa pessoa
+    ) {
+        logger.info("PUT => /pessoas");
+        this.pessoaService.updatePessoa(pessoa, id);
+        return ResponseEntity.status(204).build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete (
+            @PathVariable("id") Long id
+    ) {
+        logger.info("Delete => /pessoas");
+        this.pessoaService.delete(id);
+        return ResponseEntity.status(204).build();
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,4 +9,16 @@ CREATE TABLE veiculos(
 );
 
 INSERT INTO veiculos (marca, modelo, placa, ano, cor, valor_diaria) VALUES
-    ('Chevrolet', 'Celta', 'ABC-1234', 2010, 'preto', 100.00)
+    ('Chevrolet', 'Celta', 'ABC-1234', 2010, 'preto', 100.00);
+
+
+CREATE TABLE pessoas(
+     id BIGINT AUTO_INCREMENT PRIMARY KEY,
+     nome VARCHAR(255),
+     cpf VARCHAR(255),
+     telefone VARCHAR(255),
+     email VARCHAR(255)
+);
+
+INSERT INTO pessoas (nome, cpf, telefone, email) VALUES
+    ('Rafael Teles', '12345678901', '48988887777', 'rafael@gmail.com');
