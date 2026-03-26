50acb4c9fbb311136a2744672824404a1c637ff0
Rafael Teles Espindola <rafatespindola@gmail.com>
2026-03-26 20:00:55 -0300

    feat: confirações swagger e documentação parcial

--- src/main/java/br/com/fiap/locatech/locatech/dtos/AluguelRequestDTO.java
+++ src/main/java/br/com/fiap/locatech/locatech/dtos/AluguelRequestDTO.java
@@ -1,13 +1,16 @@
 package br.com.fiap.locatech.locatech.dtos;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 
 public record AluguelRequestDTO(
+        @Schema(description = "Id da pessoa que aluga o veículo")
         @NotNull(message = "O id da pessoa não pode ser null")
         Long pessoaId,
         @NotNull(message = "O id do veículo não pode ser null")
+        @Schema(description = "Id do veículo que está sendo alugado")
         Long veiculoId,
         LocalDate dataInicio,
         LocalDate dataFim
